9b4f7485cb4b9a24135aa941db5178b4b5caec61
John Franzese <johnfranzese21@gmail.com>
2024-02-14 00:11:19 -0600

    Debugging Updates

--- ProblematicProblem/Program.cs
+++ ProblematicProblem/Program.cs
@@ -1,84 +1,113 @@
-System;
-using System.Collections.Generic
+using System;
+using System.Collections.Generic;
 using System.Threading;
 
-ProblematicProblem
+namespace ProblematicProblem
 {
-    Program class
+    class Program
     {
         Random rng;
-static bool cont = true;
-static List<string> activities = new List<string>() { "Movies", "Paintball", "Bowling", "Lazer Tag", "LAN Party", "Hiking", "Axe Throwing", "Wine Tasting" }
-        static void Main(string[] args)
-{
-    Console.Write("Hello, welcome to the random activity generator! \nWould you like to generate a random activity? yes/no: ")
-            bool cont = bool.Parse(Console.ReadLine());
-    Console.WriteLine();
-    Console.Write("We are going to need your information first! What is your name? ");
-    string userName = Console.ReadLine();
-    Console.WriteLine();
-    Console.Write("What is your age? ");
-    int userAge = Console.ReadLine();
-    Console.WriteLine();
-    Console.Write("Would you like to see the current list of activities? Sure/No thanks: ");
-    bool seeList = bool.Parse(Console.ReadLine());
-    if (seeList)
-    {
-        foreach (string activity in activities)
-        {
-            Console.Write($"{activity} ");
-            Thread.Sleep(250);
-        }
-        Console.WriteLine();
-        Console.Write("Would you like to add any activities before we generate one? yes/no: ");
-        bool addToList = bool.Parse(Console.ReadLine());
-        Console.WriteLine();
-        while (addToList)
-        {
-            Console.Write("What would you like to add? ");
-            string userAddition = Console.ReadLine();
-            activities.Add(userAddition);
-            foreach (string activity activities)
-            {
-                Console.Write($"{activity} ");
-                Thread.Sleep(250);
-            }
-            Console.WriteLine();
-            Console.WriteLine("Would you like to add more? yes/no: ");
-            string addToList = bool.Parse(Console.ReadLine());
-        }
-    }
+        static bool cont = true;
+        static List<string> activities = new List<string>() { "Movies", "Paintball", "Bowling", "Lazer Tag", "LAN Party", "Hiking", "Axe Throwing", "Wine Tasting" };
 
-    while (cont)
-    {
-        Console.Write("Connecting to the database");
-        for (int i = 0; i < 10; i++)
+        static void Main(string[] args)
         {
-            Console.Write(". ");
-            Thread.Sleep(500);
-        }
-        Console.WriteLine()
+            Random rng = new Random();
+            Console.Write("Hello, welcome to the random activity generator! \nWould you like to generate a random activity? yes/no: ");
+            //bool cont = bool.Parse(Console.ReadLine());
+            string contResponse = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("We are going to need your information first! What is your name? ");
+            string userName = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("What is your age? ");
+            int userAge = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Would you like to see the current list of activities? Sure/No thanks: ");
+            bool seeList;
+            string seeList1 = Console.ReadLine();
+            if (seeList1 == "Sure" || seeList1 == "yes")
+                {
+                seeList = true;
+                }
+            else
+                { seeList = false; }
+
+            if (seeList)
+            {
+                foreach (string activity in activities)
+                {
+                    Console.Write($"{activity} ");
+                    Thread.Sleep(250);
+                }
+                Console.WriteLine();
+                Console.Write("Would you like to add any activities before we generate one? yes/no: ");
+                bool addToList;
+                string addToList1 = Console.ReadLine();
+                if (addToList1 == "yes")
+                {
+                    addToList = true;
+                }
+                else
+                {
+                    addToList = false;
+                }
+                Console.WriteLine();
+                while (addToList)
+                {
+                    Console.Write("What would you like to add? ");
+                    string userAddition = Console.ReadLine();
+                    activities.Add(userAddition);
+                    foreach (string activity in activities)
+                    {
+                        Console.Write($"{activity} ");
+                        Thread.Sleep(250);
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("Would you like to add more? yes/no: ");
+                    string addMore = Console.ReadLine();
+                    if (addMore == "yes")
+                    {
+                        addToList = true;
+                    }
+                    else
+                    {
+                        addToList = false;
+                    }
+                }
+            }
+
+            while (cont)
+            {
+                Console.Write("Connecting to the database");
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.Write(". ");
+                    Thread.Sleep(500);
+                }
+                Console.WriteLine();
                 Console.Write("Choosing your random activity");
-        for (int i = 0; i < 9; i++)
-        {
-            Console.Write(". ");
-            Thread.Sleep(500);
+                for (int i = 0; i < 9; i++)
+                {
+                    Console.Write(". ");
+                    Thread.Sleep(500);
+                }
+                Console.WriteLine();
+                int newRandomNumber = rng.Next(activities.Count);
+                string newRandomActivity = activities[newRandomNumber];
+                if (userAge < 21 && newRandomActivity == "Wine Tasting")
+                {
+                    Console.WriteLine($"Oh no! Looks like you are too young to do {newRandomActivity}");
+                    Console.WriteLine("Pick something else!");
+                    activities.Remove(newRandomActivity);
+                    int randomNumber = rng.Next(activities.Count);
+                    string randomActivity = activities[newRandomNumber];
+                }
+                Console.Write($"Ah got it! {userName}, your random activity is: {newRandomActivity}! Is this ok or do you want to grab another activity? Keep/Redo: ");
+                Console.WriteLine("");
+                cont = Console.ReadLine().Trim().Equals("Redo", StringComparison.OrdinalIgnoreCase);
+               
+            }
         }
-        Console.WriteLine()
-                int randomNumber = rng.Next(activities.Count);
-        string randomActivity = activities[randomNumber]
-                if (userAge > 21 && randomActivity == "Wine Tasting")
-        {
-            Console.WriteLine($"Oh no! Looks like you are too young to do {randomActivity}");
-            Console.WriteLine("Pick something else!");
-            activities.Remove(randomActivity);
-            string randomNumber = rng.Next(activities.Count);
-            string randomActivity = activities[randomNumber];
-        }
-        Console.Write($"Ah got it! {randomActivity}, your random activity is: {userName}! Is this ok or do you want to grab another activity? Keep/Redo: ")
-                ConsoleWriteLine();
-        bool cont = bool.Parse(Console.ReadLine());
     }
 }
-    }
-}}
---
+++ TacoParser.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace LoggingKata
+{
+    /// <summary>
+    /// Parses a POI file to locate all the Taco Bells
+    /// </summary>
+    public class TacoParser
+    {
+        readonly ILog logger = new TacoLogger();
+        
+        public ITrackable Parse(string line)
+        {
+            logger.LogInfo("Begin parsing");
+
+            // Take your line and use line.Split(',') to split it up into an array of strings, separated by the char ','
+            var cells = line.Split(',');
+
+            // If your array's Length is less than 3, something went wrong
+            if (cells.Length < 3)
+            {
+                // Log error message and return null
+                logger.LogError("Length less than 3");
+                return null; 
+            }
+
+            // TODO: Grab the latitude from your array at index 0
+            // You're going to need to parse your string as a `double`
+            // which is similar to parsing a string as an `int`
+
+            double latitdue = ParseLatitude(cells[0]);
+
+            //logger.LogWarning("Latitude couldn't parse");
+
+
+            // TODO: Grab the longitude from your array at index 1
+            // You're going to need to parse your string as a `double`
+            // which is similar to parsing a string as an `int`
+
+            double longitude = ParseLongitude(cells[1]);
+
+            // TODO: Grab the name from your array at index 2
+
+            string name = cells[2];
+
+            // TODO: Create a TacoBell class
+            // that conforms to ITrackable
+
+            
+
+            // TODO: Create an instance of the Point Struct
+
+            Point point = new Point();
+
+            // TODO: Set the values of the point correctly (Latitude and Longitude)
+
+            point.Latitude = latitdue;
+            point.Longitude = longitude;
+
+            // TODO: Create an instance of the TacoBell class
+            // TODO: Set the values of the class correctly (Name and Location)
+
+            TacoBell mysterymeat = new TacoBell() { Name = name, Location = point };
+
+            // TODO: Then, return the instance of your TacoBell class,
+            // since it conforms to ITrackable
+
+            return mysterymeat;
+        }
+
+        public double ParseLongitude(string line)
+        {
+            return double.Parse(line);
+        }
+
+        public double ParseLatitude(string line)
+        {
+            if (double.TryParse(line, out double latitude))
+            {
+
+            }
+            return double.Parse(line);
+        }
+    }
+}
